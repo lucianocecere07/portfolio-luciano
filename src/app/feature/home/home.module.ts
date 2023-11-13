600c683263063d6a4f26058a8a56025f5172159c
lucianocecere07 <cecereluciano97@gmail.com>
2023-11-13 15:57:55 +0100

    Portfolio-type

--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import {RouterLink} from "@angular/router";
+import {NgxTypedJsModule} from "ngx-typed-js";
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
-  imports: [
-    CommonModule,
-    RouterLink,
-  ]
+    imports: [
+        CommonModule,
+        RouterLink,
+        NgxTypedJsModule,
+    ]
 })
 export class HomeModule { }
--- src/app/feature/home/home/home.component.ts
+++ src/app/feature/home/home/home.component.ts
@@ -1,31 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, interval } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   presentationsItems: string[] = ["Java Developer", "Angular Developer", "Spring Boot Developer", "Full Stack Developer"];
-  currentIndex = 0;
 
   cv: string = "assets/home/cv.pdf";
 
   linkedinLink: string = "https://www.linkedin.com";
   emailLink: string = "mailto:mail@email.com";
   githubLink: string = "https://github.com";
-
-  ngOnInit() {
-    const interval$ = interval(2500);
-
-    interval$
-      .pipe(
-        map(() => (this.currentIndex = (this.currentIndex + 1) % this.presentationsItems.length))
-      )
-      .subscribe();
-  }
 
   download() {
     window.open(this.cv, "_blank");
